891d38e6045c5de60176b62f402df1d80086e254
om-dev007 <officialomnarayan@gmail.com>
2026-03-06 16:36:07 +0530

    cookie parser add

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,13 @@
 import e from "express";
 import router from "./routes/notes.routes.js";
 import cors from "cors"
+import cookieParser from 'cookie-parser'
 import userRoutes from "./routes/auth.routes.js";
 
 const app = e();
 app.use(cors());
 app.use(e.json());
+app.use(cookieParser())
 app.use("/notes", router);
 app.use("/auth", userRoutes)
 
