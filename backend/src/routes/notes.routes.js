ea1e7d2b2150e95ecbf16e993c84c863994e0a65
om-dev007 <officialomnarayan@gmail.com>
2026-03-06 16:40:35 +0530

    notes routes update for role based access

--- backend/src/routes/notes.routes.js
+++ backend/src/routes/notes.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { createNotes, deleteNote, getNotes, updateNote } from "../controllers/notes.controllers.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = Router();
 
-router.post("/", createNotes);
-router.get("/", getNotes);
-router.patch("/:id", updateNote);
-router.delete("/:id", deleteNote)
+router.post("/", authMiddleware ,createNotes);
+router.get("/", authMiddleware ,getNotes);
+router.patch("/:id", authMiddleware, updateNote);
+router.delete("/:id", authMiddleware, deleteNote)
 
 export default router;
